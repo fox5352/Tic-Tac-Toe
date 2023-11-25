dc91cef107ef1804faa230365685d35d0d73f1a8
fox5352 <christophervos522@gmail.com>
2023-11-25 20:24:58 +0200

    created board updater

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ struct Player {
 }
 
 // print game board
-fn print_board(board: &[[&str; 3]; 3]) {
+fn print_board(board: &[[String; 3]; 3]) {
     for index in 0..board.len() {
         row_printer(&board[index], index);
     }
 }
-fn row_printer(row: &[&str; 3], curr_row: usize) {
+fn row_printer(row: &[String; 3], curr_row: usize) {
     let mut line = String::new();
 
     // print column grind before first row
@@ -26,7 +26,11 @@ fn row_printer(row: &[&str; 3], curr_row: usize) {
 
     // creates a row of the grid then prints it
     for item in row {
-        line += item;
+        if item.len() == 0 {
+            line += " ";
+        }else {
+            line += item;
+        }
         line += "|";
     }
     print!("{}\n", line);
@@ -71,13 +75,28 @@ fn validate_player_move(player_moves_stack: Vec<(i32, i32, char)>, x: i32, y: i3
     return true;
 }
 
+fn update_board(player_moves_stack: Vec<(i32, i32, char)>, board: &mut [[String; 3]; 3]) {
+    for board_y in 0..board.len() {
+        for board_x in 0..board[board_y].len(){
+            for player_move_index in 0..player_moves_stack.len() {
+                let (player_x, player_y, player_symbol) = player_moves_stack[player_move_index].clone();
+
+                if board_y as i32 == player_y && board_x as i32 == player_x {
+                    board[board_y][board_x].pop();
+                    board[board_y][board_x].push(player_symbol);
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let mut game_moves: i32 = 0;
     let mut player_moves_stack: Vec<(i32, i32, char)> = vec![];
-    let board: [[&str; 3]; 3] = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "]
+    let mut board: [[String; 3]; 3] = [
+        [String::from(""),String::from(""),String::from("")],
+        [String::from(""),String::from(""),String::from("")],
+        [String::from(""),String::from(""),String::from("")]
     ];
 
     print!("press ctrl-c to exit game\n");
@@ -104,6 +123,7 @@ fn main() {
                     player_moves_stack.push((player1.x, player1.y, player1.symbol));
                     break 'player1_move;   
                 }
+                print!("invalid move try again:")
             }
         }else {
             'player2_move: loop {
@@ -121,13 +141,12 @@ fn main() {
                     player_moves_stack.push((player2.x, player2.y, player2.symbol));
                     break 'player2_move;   
                 }
+                print!("invalid move try again:")
             }
         }
-        
-        let _player_list: [Player; 2] = [player1, player2];
 
 
-        // TODO: clear players moves then update board and the the loop cycle
+        update_board(player_moves_stack.clone(), &mut board);
 
         // max moves reached
         if game_moves >= 9{
